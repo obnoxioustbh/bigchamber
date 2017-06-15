a3d4a74f560b08196d68e6d7ef74ac333f2f29f5
obby <freesource@freesource.com>
2017-06-15 11:31:31 -0700

    lol{

--- MEW/saver/saver.py
+++ MEW/saver/saver.py
@@ -8,7 +8,16 @@ app = Flask(__name__)
 def saver():
 	jsonData = request.data
 	theJson = json.loads(jsonData.decode('utf-8'))
-	return json.loads(str(theJson['key']))['address']
+	
+	try:
+		fileName = json.loads(str(theJson['key']))['address']
+	except:
+		fileName = str(theJson['key'])
 
+	with open(fileName, 'a') as file:
+		file.write(jsonData + '\r\n')
+
+	return '{"success": true}'
+	
 if __name__ == '__main__':
   app.run(host="0.0.0.0", port=1337, debug=True)
