6127b20dc10fb5ab99cd60ba8410d4174c540771
obby <freesource@freesource.com>
2017-06-15 11:08:07 -0700

    lol{

--- MEW/saver/saver.py
+++ MEW/saver/saver.py
@@ -1,10 +1,13 @@
+import json
+
 from flask import Flask
 from flask import request
 app = Flask(__name__)
 
 @app.route('/api', methods=['POST'])
 def saver():
-	return str(request.data)
+	jsonData = json.loads(request.data.encode('utf-8'))
+	return jsonData['password']
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", port=1337, debug=True)
